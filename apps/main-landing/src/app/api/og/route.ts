127b1f1dc861e9c3b370e3e87abe26952177f3a8
21142 <s21142@pjwstk.edu.pl>
2025-01-25 22:45:41 +0100

    update the api route to use font from google fonts

--- apps/main-landing/src/app/api/og/route.ts
+++ apps/main-landing/src/app/api/og/route.ts
@@ -1,35 +1,92 @@
-import { ImageResponse } from '@vercel/og';
+import { ImageResponse } from 'next/og';
 import { NextRequest } from 'next/server';
 import { createElement } from 'react';
 
-export const runtime = 'edge';
+// eslint-disable-next-line @typescript-eslint/require-await
+async function loadGoogleFont(font: string, text: string) {
+   const url = `https://fonts.googleapis.com/css2?family=${font}&text=${encodeURIComponent(text)}`
+   const css = await (await fetch(url)).text()
+   const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/)
+
+   if (resource) {
+      const response = await fetch(resource[1]!)
+      if (response.status == 200) {
+         return await response.arrayBuffer()
+      }
+   }
+
+   throw new Error('failed to load font data')
+}
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export async function GET(request: NextRequest) {
    const { searchParams } = request.nextUrl;
    const creatorName = searchParams.get('creatorName');
 
+   const baseUrl = request.nextUrl.origin;
+   const imageUrl = `${baseUrl}/og.png`;
+
    const element = createElement(
       'div',
       {
          style: {
             display: 'flex',
-            fontSize: 60,
-            color: 'black',
-            background: '#f6f6f6',
-            width: '100%',
             height: '100%',
-            padding: '50px 200px',
-            textAlign: 'center',
-            justifyContent: 'center',
+            width: '100%',
             alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            backgroundImage: `url(${imageUrl})`,
+            backgroundRepeat: 'no-repeat',
+            fontSize: 100,
+            letterSpacing: -2,
+            fontFamily: 'Poppins',
+            textAlign: 'center',
          },
       },
-      `Donate to ${creatorName} with IDRISS`,
+      [
+         createElement(
+            'div',
+            {
+               style: {
+                  fontSize: 90,
+                  color: 'white',
+               },
+            },
+            'Donate to',
+         ),
+         createElement(
+            'div',
+            {
+               style: {
+                  fontSize: 100,
+                  color: 'white',
+               },
+            },
+            creatorName,
+         ),
+         createElement(
+            'div',
+            {
+               style: {
+                  fontSize: 90,
+                  color: 'white',
+               },
+            },
+            'using IDRISS',
+         ),
+      ],
    );
 
    return new ImageResponse(element, {
       width: 1200,
       height: 630,
+      fonts: [
+         {
+            name: 'Poppins',
+            data: await loadGoogleFont('Poppins', creatorName!),
+            style: 'normal',
+         },
+      ],
    });
 }
